596af21dec49d55f052b816175777bdfced9d361
pkluber <gkluber@sbcglobal.net>
2024-12-15 22:18:59 -0600

    complete day 6

--- src/day6.zig
+++ src/day6.zig
@@ -43,14 +43,32 @@ pub fn main() !void {
         }
     }
 
+    // Define new seen array
+    const width = grid.len;
+    const height = grid[0].len;
+    var seen = try allocator.alloc(bool, width * height);
+    defer allocator.free(seen);
+
+    // Initialize seen
+    var seen_idx: usize = 0;
+    while (seen_idx < seen.len) : (seen_idx += 1) {
+        seen[seen_idx] = false;
+    }
+
     var total_steps: u32 = 0;
     var next_x: u32 = start_x;
     var next_y: u32 = start_y;
     var current_dir: u32 = 0;
     while (true) {
+        // std.debug.print("{},{}\n", .{ next_x, next_y });
         // Check end condition
         if (next_y >= grid.len or next_x >= grid[next_y].len)
             break;
+
+        if (!seen[next_y * width + next_x])
+            total_steps += 1;
+
+        seen[next_y * width + next_x] = true;
 
         const x = switch (current_dir) {
             0, 2 => next_x,
@@ -72,13 +90,93 @@ pub fn main() !void {
             if (char == '#') {
                 current_dir += 1;
                 current_dir %= 4;
+            } else {
+                next_x = x;
+                next_y = y;
             }
+        } else {
+            next_x = x;
+            next_y = y;
         }
-
-        next_x = x;
-        next_y = y;
-        total_steps += 1;
     }
 
     std.debug.print("Day 6 Part 1 final answer: {}\n", .{total_steps});
+
+    // Define new seen_dir array
+    var seen_dir = try allocator.alloc(bool, width * height * 4);
+    defer allocator.free(seen_dir);
+
+    // Run with different obstruction positions
+    var num_obstruction: u32 = 0;
+    var oy: usize = 0;
+    while (oy < grid.len) : (oy += 1) {
+        var ox: usize = 0;
+        while (ox < grid[oy].len) : (ox += 1) {
+            if (grid[oy][ox] == '#')
+                continue;
+
+            grid[oy][ox] = '#';
+
+            // Reinitialize seen_dir
+            var seen_dir_idx: usize = 0;
+            while (seen_dir_idx < seen_dir.len) : (seen_dir_idx += 1) {
+                seen_dir[seen_dir_idx] = false;
+            }
+
+            next_x = start_x;
+            next_y = start_y;
+            current_dir = 0;
+            var found_loop = false;
+            while (true) {
+                // Check end condition -- no loop
+                if (next_y >= grid.len or next_x >= grid[next_y].len)
+                    break;
+
+                const idx = 4 * (next_y * width + next_x) + current_dir;
+
+                if (seen_dir[idx]) {
+                    found_loop = true;
+                    break;
+                }
+
+                seen_dir[idx] = true;
+
+                const x = switch (current_dir) {
+                    0, 2 => next_x,
+                    1 => next_x + 1,
+                    3 => next_x -% 1,
+                    else => unreachable,
+                };
+
+                const y = switch (current_dir) {
+                    1, 3 => next_y,
+                    0 => next_y -% 1,
+                    2 => next_y + 1,
+                    else => unreachable,
+                };
+
+                // Check if obscruction ahead
+                if (y < grid.len and x < grid[y].len) {
+                    const char = grid[y][x];
+                    if (char == '#') {
+                        current_dir += 1;
+                        current_dir %= 4;
+                    } else {
+                        next_x = x;
+                        next_y = y;
+                    }
+                } else {
+                    next_x = x;
+                    next_y = y;
+                }
+            }
+
+            if (found_loop)
+                num_obstruction += 1;
+
+            grid[oy][ox] = '.';
+        }
+    }
+
+    std.debug.print("Day 6 Part 2 final answer: {}\n", .{num_obstruction});
 }
